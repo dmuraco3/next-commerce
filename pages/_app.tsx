6e1e0cdcfa4483609cbc5baa7edfeda15a351d2b
dmuraco3 <dylanjmuraco@gmail.com>
2021-12-28 21:26:05 -0500

    Navbar works

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Navbar />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
