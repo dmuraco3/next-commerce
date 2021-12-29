224cd40b07d669dbcc1c85dcccbcceb37b1cb7a2
Best-Code <cpmraygun@gmail.com>
2021-12-28 23:12:17 -0400

    Fixed index.tsx on footer

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import {Footer} from "../components/footer/index"
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+    return (
+        <div>
+        <Footer/>
+        </div>
+    )  
+}
+
+export default Home
