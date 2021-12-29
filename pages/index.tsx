0c6fc34ea530fc93a9b35a84d8a4c74a51ac19fe
Best-Code <cpmraygun@gmail.com>
2021-12-29 10:35:55 -0400

    Remove useless imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import {Footer} from "../components/footer/index"
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
