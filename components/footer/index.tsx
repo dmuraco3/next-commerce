f2a877797bc50d8ec100776181b1c8ae4f732ddd
Best-Code <cpmraygun@gmail.com>
2021-12-29 10:32:40 -0400

    Footer branch updated

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,60 +1,51 @@
-import styles from './footer.module.css'
+import { FaFacebookF, FaInstagram, FaPinterestP, FaTwitter } from "react-icons/fa";
 
 export const Footer: React.FC = () => {
-    return (<div>
-        {/* LEFT HALF */}
-        <div className={styles.float_h}>
-            <div>
-                <div className={styles.topLeft}>
-                    <h1>
-                        HAPPY DAYS CLOTHING
-                    </h1>
-                    <p>
-                        contact@site.com
-                        123-456-7890
-                    </p>
-                </div>
-                <div className={styles.bottomLeft}>
-                    <p>Facebook</p>
-                    <p>Facebook</p>
-                    <p>Facebook</p>
-                    <p>Facebook</p>
-                </div>
-            </div>
-        </div>
+    return (
+        <footer className="border border-black box-border w-full">
+            <div className="flex">
+                <div className="flex flex-col bg-black items-center w-3/12 justify-center text-white font-mono py-10 border border-black">
+                    <p className="text-2xl mb-5">HAPPY DAYS CLOTHING</p>
+                    <p>contact@site.com</p>
+                    <p>123-456-7890</p>
 
-        {/* RIGHT HALF */}
-        <div className={styles.empty}>
-            <div className={styles.float_h}>
-                <div>
-                    <div className={styles.topRight}>
-                        <ul>
-                            <li>
-                                Shop
-                                <ul>
-                                    <li>
-                                        New
-                                    </li>
-                                    <li>
-                                        Men
-                                    </li>
-                                    <li>
-                                        Women
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
+                </div>
+                <div className="flex justify-around w-9/12 py-8 border border-black">
+                    <div className="flex flex-col items-start">
+                        <span className="my-2 text-lg font-semibold">Shop</span>
+                        <span className="my-1">New</span>
+                        <span className="my-1">Men</span>
+                        <span className="my-1">Women</span>
                     </div>
-                    <div className={styles.bottomRight}>
-                        <p>
-                            © 2021 by Happy Days LLC.
-                        </p>
+                    <div className="flex flex-col items-start">
+                        <span className="my-2 text-lg font-semibold">Our Store</span>
+                        <span className="my-1">About Us</span>
+                        <span className="my-1">Subscribe</span>
+                        <span className="my-1">FAQ</span>
+                    </div>
+                    <div className="flex flex-col items-start">
+                        <span className="my-2 text-lg font-semibold">Terms & Conditions</span>
+                        <span className="my-1">Store Policy</span>
+                        <span className="my-1">Shipping & Returns</span>
+                        <span className="my-1">Payment Methods</span>
                     </div>
                 </div>
-            </div>
-        </div>
 
-    </div>)
+            </div>
+            <div className="flex">
+                <div className="flex justify-around py-4 w-3/12 px-4 border border-black">
+                    <FaFacebookF size={26}/>
+                    <FaInstagram size={26} />
+                    <FaPinterestP size={26} />
+                    <FaTwitter size={26} />
+
+                </div>
+                <div className="flex justify-center items-center w-9/12 bg-primary border-black border">
+                    <span>© 2021 by Happy Days LLC.</span>
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer;
