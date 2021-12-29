267bd0195efada119a7402b58f79af165061715f
dmuraco3 <dylanjmuraco@gmail.com>
2021-12-28 20:48:02 -0500

    tailwind init

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
     return (
         <div>
-		<h1>Hello World!</h1>
+            <h1>Hello World!</h1>
         </div>
     )  
 }
