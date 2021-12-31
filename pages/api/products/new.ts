c553a74d0584cebfa0f29eec3d468b803060f097
dmuraco3 <dylanjmuraco@gmail.com>
2021-12-30 20:30:20 -0500

    product variants can now be added to database

---
+++ pages/api/products/new.ts
@@ -0,0 +1,83 @@
+import { PrismaClient, Product_Variant, VariantType } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    const productToAdd: {name: string, price: number, tags: string[], category: string, ProductVariants: {
+        name: string,
+        type: VariantType,
+        price: number
+        sku: string
+    }[] } = {
+        name: "Ice Cream Shirt",
+        price: 190,
+        tags: [
+            "Mens t-shirt",
+            "t shirt",
+            "t-shirt"
+        ],
+        category: "Mens Shirts",
+        ProductVariants: [
+            {
+                type: 'Size',
+                name: 'Small',
+                price: 190,
+                sku: 'Mens/IceCreamShirt/Small'
+            },
+            {
+                type: 'Size',
+                name: 'Medium',
+                price: 190,
+                sku: 'Mens/IceCreamShirt/Medium '
+            }
+        ]
+    }
+
+    const tags = ['t shirt', 't-shirt', 'black', 'medium']
+
+    const product = await prisma.product.create({
+        data: {
+            name: productToAdd.name,
+            price: productToAdd.price,
+            tags: {
+                connectOrCreate: productToAdd.tags.map(tag => {
+                    return {
+                        where: {
+                            name: tag
+                        },
+                        create: {
+                            name: tag
+                        }
+                    }
+                })
+            },
+            category: {
+                connectOrCreate: {
+                    create: {
+                        name: productToAdd.category
+                    },
+                    where: {
+                        name: productToAdd.category
+                    }
+                }
+            },
+            ProductVariants: {
+                createMany: {
+                    data: productToAdd.ProductVariants.map(variant => {
+                        return {
+                            ...variant,
+                        }
+                    })
+                }
+            }
+
+
+        }
+    })
+
+    res.status(200).json(product)
+}
