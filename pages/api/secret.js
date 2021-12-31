0b706ac4c29cd4662c9b6c91516e0e4e31f38e5a
Best-Code <cpmraygun@gmail.com>
2021-12-31 14:43:23 -0500

    Cleansing

--- pages/api/secret.js
+++
@@ -1,9 +0,0 @@
-export const Secret = async (req, res) => {
-    const intent = await fetch("http://localhost:3000/api/create-payment-intent", {
-            Method: "GET",
-    }).then(res => res.json())
-
-    res.json({clientSecret: intent.client_secret})
-}
-
-export default Secret
