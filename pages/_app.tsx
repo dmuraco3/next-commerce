c8a5b20cde6f632b91fc1bd4f603475968d2886d
Best-Code <cpmraygun@gmail.com>
2021-12-31 13:35:24 -0500

    Removes Scroll Bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,42 +2,41 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar'
-import {SessionProvider} from 'next-auth/react'
+import { SessionProvider } from 'next-auth/react'
 import Footer from '../components/footer'
 import Banner from "../components/banner"
 import LogoBanner from '../components/logo_banner'
 
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from '../stores/store'
 
 function MyApp({
   Component,
-  pageProps: {session, ...pageProps} 
-  }: AppProps) {
+  pageProps: { session, ...pageProps }
+}: AppProps) {
   return <div>
     <SessionProvider session={session}>
-        
+
 
       <Provider store={store}>
 
         <div className="flex flex-col min-h-screen">
-          
-          <Banner message="Sale is on! 25% off sitewide using SUMMER25 at checkout!"/>
+
+          <Banner message="Sale is on! 25% off sitewide using SUMMER25 at checkout!" />
           <Navbar />
-          
+
           <div className="flex-1">
             <Component {...pageProps} />
 
           </div>
-          <div className="flex-1 flex items-end">
 
-            <Footer />
-          </div>
-
+        </div>
+        <div className="flex-1 flex items-end">
+          <Footer />
         </div>
 
       </Provider>
-      
+
     </SessionProvider>
   </div>
 }
